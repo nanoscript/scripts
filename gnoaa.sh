1c54e8b519ca4a165a3c4df62c4f1ddb55465a17
xtechgeek <xclusive585@gmail.com>
2014-12-26 18:58:06 -0500

    Update gnoaa.sh

--- gnoaa.sh
+++ gnoaa.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 # GNOAA- a quick script to provide spoken NOAA forecasts (from festival) on GNU based systems.
-#
 # This is intended to blatently mimic NOAA weather radio broadcasts using GNU software.
 #
 # Requires package "Festival" be installed and in working order with sound.
@@ -11,12 +10,8 @@
 # will be pointed to a URL like the example, which is a .txt file.
 
 
-
 # change this URL as necessary for your needs:
 NOAAURL=http://weather.noaa.gov/pub/data/forecasts/zone/ny/nyz003.txt
-
-
-
 
 
 # this script will leave a file named "lastWeather.txt" in whatever directory the script is
@@ -34,8 +29,6 @@ A6="\"$A4\""  #places quotes around our text so we can pipe it into festival:
 
 echo "(SayText $A6)" | festival &    #runs our voice command in background with festival
 
-
-# wishlist, work on sed to make the input to festival a little (a lot?) cleaner.
+# wishlist, work on the sed commands to make the input to festival a little (a lot?) cleaner.
 # work on the festival command to obtain a voice that works a little better for this task
 # and that more closely resembles at least one NOAA weather radio voice.
-
